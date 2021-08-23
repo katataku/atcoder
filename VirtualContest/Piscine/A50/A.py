45543487e0bd07a90da10a47454ff0bcd265f781
katataku <katataku.dev@gmail.com>
2021-08-23 22:31:34 +0900

    A50

---
+++ VirtualContest/Piscine/A50/A.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import math
+import heapq
+import sys
+import itertools
+from collections import deque
+
+sys.setrecursionlimit(1000000)
+MOD = 10 ** 9 + 7
+n, k = map(int, input().split())
+
+s = input()
+
+c = [{} for j in range(len(s) + 1)]
+
+for i in range(26):
+    c[n][i] = -1
+for i in range(len(s)):
+    i = len(s) - i - 1
+    for alphabet in range(26):
+        if ord(s[i]) - 97 == alphabet:
+            c[i][alphabet] = i
+        else:
+            c[i][alphabet] = c[i + 1][alphabet]
+
+
+def solve(n, acc):
+    for i in range(26):
+        if -1 != c[n][i] and len(s) - (c[n][i]) + acc >= k and acc + 1 <= k:
+            ans = chr(97 + i) + solve(c[n][i] + 1, acc + 1)
+            return ans
+            if len(ans) == k - acc:
+                return ans
+    return ""
+
+
+print(solve(0, 0))
