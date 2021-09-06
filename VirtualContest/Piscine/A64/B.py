56f161d2119910dafe40f68b9b8877d3f6706fd6
katataku <katataku.dev@gmail.com>
2021-09-06 09:29:44 +0900

    A64

---
+++ VirtualContest/Piscine/A64/B.py
@@ -0,0 +1,39 @@
+n = int(input())
+
+c = list((input().split()))
+path = {}
+for i in range(n - 1):
+    a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+    path[a] = path.get(a, [])
+    path[b] = path.get(b, [])
+    path[a].append(b)
+    path[b].append(a)
+
+dp = [[0 for i in range(3)] for j in range(n)]
+
+
+def dfs(node, pre):
+    if pre != -1 and len(path[node]) == 1:
+        if c[node] == "a":
+            dp[node][0] = 1
+        else:
+            dp[node][1] = 1
+    else:
+        for next in path[node]:
+            if next != pre:
+                dfs(next, node)
+                if c[node] == "a":
+                    dp[node][0] *= dp[next][0] + dp[next][2]
+                else:
+                    dp[node][1] *= dp[next][0] + dp[next][2]
+                dp[node][2] += dp[next][0] + dp[next][1] + dp[next][2] * 2
+        if c[node] == "a":
+            dp[node][2] -= dp[node][0]
+        else:
+            dp[node][2] -= dp[node][1]
+
+
+dfs(0, -1)
+print(dp[0][2])
